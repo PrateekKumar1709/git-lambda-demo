777edd217533b96e59bf230f4bd144f7a75d4091
Prateek Kumar <85689959+PrateekKumar1709@users.noreply.github.com>
2022-08-03 14:06:36 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from git Lambda!')
+        'body': json.dumps('Hello from git Lambda!!')
     }
 
 
