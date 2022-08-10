79a6116bbb3e5c573fd10b1e67a8c1e2c2a38e92
Prateek Kumar <85689959+PrateekKumar1709@users.noreply.github.com>
2022-08-10 19:42:10 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from git Lambda demo')
+        'body': json.dumps('Hello from git Lambda demo during cloud class')
     }
 
 
